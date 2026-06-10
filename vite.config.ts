056a23a2c4e3d2c58449ab95c97ec63fce5da01e
Leul Ayfokru <abateisking@gmail.com>
2026-06-10 09:09:02 +0300

    LOGO fixed, removed the issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
   plugins: [
@@ -18,6 +19,9 @@ export default defineConfig({
       },
     }),
     react(),
+    nitro({
+      preset: "vercel",
+    }),
   ],
   resolve: {
     alias: {
